8cb569184582a258c07918bb7e099d6b256a2136
Kramer Jensen <Kramerjensend@gmail.com>
2020-10-08 15:32:32 -0700

    Created function to test if txtBox have data

--- js/main.js
+++ js/main.js
@@ -3,5 +3,18 @@ window.onload = function () {
     formBtn.onclick = main;
 };
 function main() {
-    alert("Reg btn was clicked");
+    isTextPresent("first-name", "First name is required!");
+    isTextPresent("last-name", "Last name is required!");
+}
+function isTextPresent(id, errMsg) {
+    var txtBox = document.getElementById(id);
+    var txtBoxValue = txtBox.value;
+    if (txtBoxValue == "") {
+        var errSpan = txtBox.nextElementSibling;
+        errSpan.innerText = errMsg;
+        return false;
+    }
+    else {
+        return true;
+    }
 }
