d069234435b28117320c48200cd6fa9f033c792f
Kramer Jensen <Kramerjensend@gmail.com>
2020-10-26 21:17:31 -0700

    Added DOB validation

--- ts/main.ts
+++ ts/main.ts
@@ -8,6 +8,24 @@ function main(): void {
     resetErrorMessages();
     isTextPresent("first-name", "First name is required!");
     isTextPresent("last-name", "Last name is required!");
+
+    // Validate date
+    let dobBox = <HTMLInputElement> document.getElementById("dob");
+    let dob = dobBox.value;
+    if (!isValidDate(dob)) {
+        let errSpan = dobBox.nextElementSibling;
+        errSpan.innerHTML = "Invalid format. ex) mm/dd/yyyy";
+    }
+}
+
+function isValidDate(input:string):boolean {
+    // validating mm/dd/yyyy and m/d/yyyy
+    // \d{1,2}\/\d{1,2}\/\d{4}
+    let pattern = /^\d{1,2}\/\d{1,2}\/\d{4}$/g
+    return pattern.test(input);
+
+    // let result = pattern.test(input);
+    // return result;
 }
 
 /**
