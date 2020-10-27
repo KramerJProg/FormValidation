de1e3175f5788dcef247fa6b8c551e948eb63bcd
Kramer Jensen <Kramerjensend@gmail.com>
2020-10-27 00:14:09 -0700

    Practice DOM manipulation

--- ts/main.ts
+++ ts/main.ts
@@ -5,6 +5,17 @@ window.onload = function() {
 }
 
 function main(): void {
+    let msgHeading = document.createElement("h2");
+    msgHeading.innerText = "Processing form";
+    msgHeading.setAttribute("class", "message");
+
+    let h1 = document.querySelector("h1");
+    h1.insertAdjacentElement("afterend", msgHeading);
+
+    setTimeout(function(){
+        msgHeading.remove();
+    }, 3000)
+
     resetErrorMessages();
     isTextPresent("first-name", "First name is required!");
     isTextPresent("last-name", "Last name is required!");
